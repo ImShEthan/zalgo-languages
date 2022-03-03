f2cb70fb0013f7f9526b51a3bbe3e1cd8298b7d0
Seii™ <62060378+SeiiCharles@users.noreply.github.com>
2022-03-03 18:23:25 +0100

    Update admins.js

--- en-US/commands/admin/admins.js
+++ en-US/commands/admin/admins.js
@@ -1,11 +1,11 @@
 const { emojis } = require("../../../../config");
 module.exports = {
-  title: (members) => `Liste des administrateurs - (${members})`,
+  title: (members) => `List of administrators - (${members})`,
 
   selectMenu: {
-    title: () => `Afficher plus de détails`,
-    firstOption: () => `Afficher les statuts`,
-    secondOption: () => `Afficher les bots vérifiés`,
-    thirdOption: () => `Afficher les membres whitelist et owners`,
+    title: () => `Show more details`,
+    firstOption: () => `Show statuses`,
+    secondOption: () => `Show certified bots`,
+    thirdOption: () => `Show whitelist members and owners`,
   },
 };
