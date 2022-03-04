42669c25b343aa88e6fd4f0a510899622dd81a13
Seii™ <62060378+SeiiCharles@users.noreply.github.com>
2022-03-04 14:28:36 +0100

    Update antilinks.js

--- en-US/commands/admin/antilinks.js
+++ en-US/commands/admin/antilinks.js
@@ -1,36 +1,36 @@
 const { emojis } = require("../../../../config");
 module.exports = {
   description: (message, guildData) => `
-**1)** Empêcher l 'envoie de liens discord\n${
+**1)** Prevent sending discord links\n${
     guildData.enableddiscordlinks
-      ? `${emojis.enabled} Activé\nDepuis le <t:${Math.round(
+      ? `${emojis.enabled} Enabled\nSince <t:${Math.round(
           guildData.since / 1000
         )}:F>`
-      : `${emojis.disabled} Désactivé`
+      : `${emojis.disabled} Disabled`
   }
 
-**2)** Empêcher l 'envoie de liens https\n${
+**2)** Prevent sending https links\n${
     guildData.enabledlinks
-      ? `${emojis.enabled} Activé\nDepuis le <t:${Math.round(
+      ? `${emojis.enabled} Enabled\nSince <t:${Math.round(
           guildData.since / 1000
         )}:F>`
-      : `${emojis.disabled} Désactivé`
+      : `${emojis.disabled} Disabled`
   }
         
-**3)** Ignorer des salons pour les liens discord `,
+**3)** Ignore channels for discord links `,
 
   listDescription: () =>
-    `**1)** Ajouter un salon à ignorer\n**2)** Enlever un salon à ignorer\n**3)** Liste des salons ignorés`,
+    `**1)** Add a channel to ignore\n**2)** Remove a channel to ignore\n**3)** List of ignored channels`,
 
-  title: (count) => `Salons ignorés - (${count})`,
+  title: (count) => `Ignored channels - (${count})`,
 
-  noFoundChannel: () => `Ce salon est introuvable !`,
+  noFoundChannel: () => `This channel can't be found!`,
 
-  addChannelQuestion: () => `Veuillez mentionner le salon à ignorer !`,
-  channelAlreadyExist: () => `Ce salon est déjà dans les salons ignorés`,
-  successAdd: (salon) => `${salon} a été ajouté aux salons ignorés !`,
+  addChannelQuestion: () => `Please mention which channel to ignore!`,
+  channelAlreadyExist: () => `This channel is already in ignored channels`,
+  successAdd: (salon) => `${salon} has been added to ignored channels!`,
 
-  removeChannelQuestion: () => `Veuillez mentionner le salon retirer !`,
-  channelAlreadyNoExist: () => `Ce salon n'est pas dans les salons ignorés`,
-  successRemove: (salon) => `${salon} a été retiré des salons ignorés !`,
+  removeChannelQuestion: () => `Please mention the channel to be removed!`,
+  channelAlreadyNoExist: () => `This channel is not in the ignored channels`,
+  successRemove: (salon) => `${salon} has been removed from ignored channels!`,
 };
