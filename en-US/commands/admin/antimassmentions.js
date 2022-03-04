b7edeba57d3e8c2540e7e18fa32a0f1aa8ded2e3
Seii™ <62060378+SeiiCharles@users.noreply.github.com>
2022-03-04 14:34:52 +0100

    Update antimassmentions.js

--- en-US/commands/admin/antimassmentions.js
+++ en-US/commands/admin/antimassmentions.js
@@ -1,36 +1,36 @@
 const { emojis } = require("../../../../config");
 module.exports = {
   description: (message, guildData) => `                     
-**1)** Empêcher l 'envoie de messages avec mentions excessives\n${
+**1)** Prevent sending messages with excessive mentions\n${
     guildData.enabled
-      ? `${emojis.enabled} Activé\nDepuis le <t:${Math.round(
+      ? `${emojis.enabled} Enabled\nSince <t:${Math.round(
           Number(guildData.since) / 1000
         )}:F>`
-      : `${emojis.disabled} Désactivé`
+      : `${emojis.disabled} Disabled`
   }
               
-**2)** Nombre de mentions maximum\n**${Number(
+**2)** Maximum number of mentions\n**${Number(
     guildData.number
-  )}** mentions maximum
+  )}** maximum mentions
   
-**3)** Ignorer des salons pour l'envoie de messages avec mentions excessives`,
+**3)** Ignore channels for sending messages with excessive mentions`,
 
   listDescription: () =>
-    `**1)** Ajouter un salon à ignorer\n**2)** Enlever un salon à ignorer\n**3)** Liste des salons ignorés`,
+    `**1)** Add a channel to ignore\n**2)** Remove a channel to ignore\n**3)** List of ignored channels`,
 
-  title: (count) => `Salons ignorés - (${count})`,
+  title: (count) => `Ignored channels - (${count})`,
 
-  noFoundChannel: () => `Ce salon est introuvable !`,
+  noFoundChannel: () => `This channel can't be found!`,
 
-  addChannelQuestion: () => `Veuillez mentionner le salon à ignorer !`,
-  channelAlreadyExist: () => `Ce salon est déjà dans les salons ignorés`,
-  successAdd: (salon) => `${salon} a été ajouté aux salons ignorés !`,
+  addChannelQuestion: () => `Please mention which channel to ignore!`,
+  channelAlreadyExist: () => `This channel is already in ignored channels`,
+  successAdd: (salon) => `${salon} has been added to ignored channels!`,
 
-  removeChannelQuestion: () => `Veuillez mentionner le salon retirer !`,
-  channelAlreadyNoExist: () => `Ce salon n'est pas dans les salons ignorés`,
-  successRemove: (salon) => `${salon} a été retiré des salons ignorés !`,
+  removeChannelQuestion: () => `Please mention the channel to be removed!`,
+  channelAlreadyNoExist: () => `This channel is not in the ignored channels`,
+  successRemove: (salon) => `${salon} has been removed from ignored channels!`,
 
-  mentionQuestion: () => `Entrez le nombre de mentions maximum par message`,
-  errorQuestion: () => `Entrez un nombre valide !`,
-  errorQuestion1: () => "Entrez un chiffre au dessus de 1 !",
+  mentionQuestion: () => `Enter the maximum number of mentions per message`,
+  errorQuestion: () => `Enter a valid number!`,
+  errorQuestion1: () => "Enter a number above 1!",
 };
