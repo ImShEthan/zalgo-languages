a6b8bc0a6a8fe9e3210065c0f42ecf811c85f43d
Mystereee <54221203+Mystereee@users.noreply.github.com>
2022-03-05 22:45:58 +0100

    Update clearsanctions.js

--- en-US/commands/moderation/clearsanctions.js
+++ en-US/commands/moderation/clearsanctions.js
@@ -1,7 +1,7 @@
 const { emojis } = require("../../../../config");
 module.exports = {
   noMemberFound: () =>
-    `${emojis.error} | Veuillez mentionner un membre valide !`,
-  isABot: () => `${emojis.error} | Cet utilisateur est un bot !`,
-  success: () => `${emojis.success} | Sanctions supprimées !`,
+    `${emojis.error} | Please mention a valid member!`,
+  isABot: () => `${emojis.error} | This user is a bot!`,
+  success: () => `${emojis.success} | Sanctions deleted!`,
 };
