096a24e83baf80e9b1ae48f69e304b42038facdc
Mystereee <54221203+Mystereee@users.noreply.github.com>
2022-03-05 01:28:58 +0100

    Update index.js

--- en-US/commands/games/index.js
+++ en-US/commands/games/index.js
@@ -1,6 +1,6 @@
 const { emojis } = require("../../../../config");
 module.exports = {
   noVoice: () =>
-    `${emojis.error} | Vous devez être connecté à un salon vocal pour lancer une activitée !`,
-  go: () => `Cliquez sur le lien d'invitation pour lancer l'activitée !`,
+    `${emojis.error} | You must be connected to a voice channel to launch an activity!`,
+  go: () => `Click on the invitation link to start the activity!`,
 };
