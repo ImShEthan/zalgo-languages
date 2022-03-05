8a155a037efb13a6acbc13cbb3a2d0d215337104
Mystereee <54221203+Mystereee@users.noreply.github.com>
2022-03-05 01:32:53 +0100

    Update leaderboard.js

--- en-US/commands/invitesystem/leaderboard.js
+++ en-US/commands/invitesystem/leaderboard.js
@@ -1,13 +1,18 @@
 const { emojis } = require("../../../../config");
 module.exports = {
-  wait: () => `${emojis.loading} | Récupération des informations...`,
-  noData: () => `${emojis.error} | Aucune invitation trouvé !`,
+  wait: () => `${emojis.loading} | Retrieving information...`,
+  noData: () => `${emojis.error} | No invitation found!`,
   desc: (lb, user, member) =>
     `${lb} **${user.tag}** - **${member.calculatedInvites}** invitations\n`,
-  title: () => `Classement des invitations`,
+  title: () => `Ranking of invitations`,
   pos: (pos) => {
-    if (pos === 1) {
-      return `Vous êtes ${pos}er dans le classement.`;
-    } else return `Vous êtes ${pos}ème dans le classement.`;
+        if (pos === 1) {
+          return `You are ${pos}st in the leaderboard.`;
+        } else if (pos === 2) {
+          return `You are ${pos}nd in the leaderboard.`;
+        } else if (pos === 3) {
+          return `You are ${pos}rd in the leaderboard.`;
+        }
+        else return`You are ${pos}th in the leaderboard.`;
   },
 };
