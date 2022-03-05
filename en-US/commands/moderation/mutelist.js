65066af86da53373cdf92aa343fbee1fad3f6dfd
Mystereee <54221203+Mystereee@users.noreply.github.com>
2022-03-05 22:54:45 +0100

    Update mutelist.js

--- en-US/commands/moderation/mutelist.js
+++ en-US/commands/moderation/mutelist.js
@@ -1,6 +1,6 @@
 const { emojis } = require("../../../../config");
 module.exports = {
-  title: (array) => `Membres mutes - (${array.length})`,
+  title: (array) => `Muted members - (${array.length})`,
   timestamp: (s) =>
     `Unmute <t:${Math.round(s.communicationDisabledUntilTimestamp / 1000)}:R>`,
 };
