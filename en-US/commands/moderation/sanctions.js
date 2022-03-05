f953df8dd0e1820abc707b3560d741d3f81fa5a9
Mystereee <54221203+Mystereee@users.noreply.github.com>
2022-03-05 22:56:47 +0100

    Update sanctions.js

--- en-US/commands/moderation/sanctions.js
+++ en-US/commands/moderation/sanctions.js
@@ -1,37 +1,37 @@
 const { emojis } = require("../../../../config");
 module.exports = {
   userNotFound: () =>
-    `${emojis.error} | Veuillez mentionner un membre valide !`,
-  userIsBot: () => `${emojis.error} | Cet utilisateur est un bot !`,
-  noSanctions: () => `Ce membre n'a aucune infraction !`,
+    `${emojis.error} | Please mention a valid member!`,
+  userIsBot: () => `${emojis.error} | This user is a bot!`,
+  noSanctions: () => `This member has no infraction!`,
   title: () => `Infractions`,
 
   old_versionDesc: (s) =>
-    `${s.type + " #" + s.case}\nModérateur: <@${s.moderator}>\nRaison: ${
+    `${s.type + " #" + s.case}\nModerator: <@${s.moderator}>\nReason: ${
       s.reason
     }`,
   desc: (s, message) => {
     if (s.type === "ban")
-      return `${s.type + " #" + s.case}\nModérateur: <@${
+      return `${s.type + " #" + s.case}\nModerator: <@${
         s.moderator
-      }>\nRaison: ${s.reason}`;
+      }>\nReason: ${s.reason}`;
     else if (s.type === "kick")
-      return `${s.type + " #" + s.case}\nModérateur: <@${
+      return `${s.type + " #" + s.case}\nModerator: <@${
         s.moderator
-      }>\nRaison: ${s.reason}`;
+      }>\nReason: ${s.reason}`;
     else if (s.type === "warn")
-      return `${s.type + " #" + s.case}\nModérateur: <@${
+      return `${s.type + " #" + s.case}\nModerator: <@${
         s.moderator
-      }>\nRaison: ${s.reason}`;
+      }>\nReason: ${s.reason}`;
     else if (s.type === "mute")
-      return `${s.type + " #" + s.case}\nModérateur: <@${
+      return `${s.type + " #" + s.case}\nModerator: <@${
         s.moderator
-      }>\nRaison: ${s.reason}\nDurée: ${
-        s.time ? message.language.convertMs(s.time) : "Infini"
+      }>\nReason: ${s.reason}\nDurée: ${
+        s.time ? message.language.convertMs(s.time) : "Infinity"
       }\n${
         s.actif
           ? s.time
-            ? "Fini: " + `<t:${Math.round(s.end / 1000)}:F>`
+            ? "End: " + `<t:${Math.round(s.end / 1000)}:F>`
             : ""
           : ""
       }`;
