476669a3d5d4181cfdac2961933d6279ed067ec5
Mystereee <54221203+Mystereee@users.noreply.github.com>
2022-03-05 01:22:26 +0100

    Update counters.js

--- en-US/commands/admin/counters.js
+++ en-US/commands/admin/counters.js
@@ -4,92 +4,92 @@ module.exports = {
 **1)** __**Counter n° 1:**__
 ${
   data.c1.enabled
-    ? `${emojis.enabled} Activé
-__Salon:__ ${
+    ? `${emojis.enabled} Enabled
+__Channel:__ ${
         message.guild.channels.cache.get(data.c1.channel)
           ? message.guild.channels.cache.get(data.c1.channel)
-          : "<:error_mys:720681441670725645> Salon introuvable !"
+          : "<:error_mys:720681441670725645> Channel not found !"
       }
-__Type de salon:__ ${
-        data.c1.channelType === "voice" ? "Salon vocal" : "Catégorie"
+__Channel type:__ ${
+        data.c1.channelType === "voice" ? "Voice channel" : "Category"
       }
-__Texte:__ ${data.c1.text}
-__Type de counter:__ ${data.c1.type}`
-    : `${emojis.disabled} Désactivé`
+__Text:__ ${data.c1.text}
+__Counter type:__ ${data.c1.type}`
+    : `${emojis.disabled} Disabled`
 }
 
 **2)** __**Counter n° 2:**__
 ${
   data.c2.enabled
-    ? `${emojis.enabled} Activé
-__Salon:__ ${
+    ? `${emojis.enabled} Enabled
+__Channel:__ ${
         message.guild.channels.cache.get(data.c2.channel)
           ? message.guild.channels.cache.get(data.c2.channel)
-          : "<:error_mys:720681441670725645> Salon introuvable !"
+          : "<:error_mys:720681441670725645> Channel not found !"
       }
-__Type de salon:__ ${
-        data.c2.channelType === "voice" ? "Salon vocal" : "Catégorie"
+__Channel type:__ ${
+        data.c2.channelType === "voice" ? "Voice channel" : "Category"
       }
-__Texte:__ ${data.c2.text}
-__Type de counter:__ ${data.c2.type}`
-    : `${emojis.disabled} Désactivé`
+__Text:__ ${data.c2.text}
+__Counter type:__ ${data.c2.type}`
+    : `${emojis.disabled} Disabled`
 }
 
 **3)** __**Counter n° 3:**__
 ${
   data.c3.enabled
-    ? `${emojis.enabled} Activé
-__Salon:__ ${
+    ? `${emojis.enabled} Enabled
+__Channel:__ ${
         message.guild.channels.cache.get(data.c3.channel)
           ? message.guild.channels.cache.get(data.c3.channel)
-          : "<:error_mys:720681441670725645> Salon introuvable !"
+          : "<:error_mys:720681441670725645> Channel not found !"
       }
-__Type de salon:__ ${
-        data.c3.channelType === "voice" ? "Salon vocal" : "Catégorie"
+__Channel type:__ ${
+        data.c3.channelType === "voice" ? "Voice channel" : "Category"
       }
-__Texte:__ ${data.c3.text}
-__Type de counter:__ ${data.c3.type}`
-    : `${emojis.disabled} Désactivé`
+__Text:__ ${data.c3.text}
+__Channel type:__ ${data.c3.type}`
+    : `${emojis.disabled} Disabled`
 }
 
 **4)** __**Counter n° 4:**__
 ${
   data.c4.enabled
-    ? `${emojis.enabled} Activé
-__Salon:__ ${
+    ? `${emojis.enabled} Enabled
+__Channel:__ ${
         message.guild.channels.cache.get(data.c4.channel)
           ? message.guild.channels.cache.get(data.c4.channel)
-          : "<:error_mys:720681441670725645> Salon introuvable !"
+          : "<:error_mys:720681441670725645> Channel not found !"
       }
-__Type de salon:__ ${
-        data.c4.channelType === "voice" ? "Salon vocal" : "Catégorie"
+__Channel type:__ ${
+        data.c4.channelType === "voice" ? "Voice channel" : "Category"
       }
-__Texte:__ ${data.c4.text}
-__Type de counter:__ ${data.c4.type}`
-    : `${emojis.disabled} Désactivé`
+__Text:__ ${data.c4.text}
+__Counter type:__ ${data.c4.type}`
+    : `${emojis.disabled} Disabled`
 }
 `,
 
   desc2: () => `
-**1)** Membres sur le serveur
-**2)** Membres (hors bots) sur le serveur
-**3)** Membres (hors utilisateurs) sur le serveur
-**4)** Catégories sur le serveur
-**5)** Salons texte sur le serveur
-**6)** Salons vocaux sur le serveur
-**7)** Boosts sur le serveur
-**8)** Membres en ligne sur le serveur
-**9)** Rôles sur le serveur
-**10)** Membres connectés en salon vocal sur le serveur
+**1)** Members on the server
+**2)** Members (excluding bots) on the server
+**3)** Members (excluding users) on the server
+**4)** Categories on the server
+**5)** Text channels on the server
+**6)** Voice channels on the server
+**7)** Boosts on the server
+**8)** Members online on the server
+**9)** Roles on the server
+**10)** Members connected in voice channels on the server
 `,
 
   type: () =>
-    `Réagissez à 📜 pour créer le counter en salon vocal\nRéagissez à 📑 pour créer le counter en catégorie`,
+    `React to 📜 to create the counter in voice channel\nReact to 📑 to create the counter in category`,
 
-  vocal: () => `Salon vocal`,
-  cat: () => `Catégorie`,
+  vocal: () => `Voice channel`,
+  cat: () => `Category`,
 
   example: () =>
-    `Entrez le nom que vous voulez pour votre counter ! **(Exemple: \`📌・Membres: {count}\`)**`,
-  error: () => `Votre nom de salon doit contenir \`{count}\` !`,
+    `Enter the name you want for your counter! **(Example: \`📌 Members: {count}\`)**`,
+  error: () => `Your channel name must contain \`{count}\` !`,
 };
